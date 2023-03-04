d3e4a8d0ada95c09da2580cb29131dea55991150
Wioleta Tyranska <wioleta.tyranska@gmail.com>
2023-03-04 23:44:35 +0100

    add basic fields

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import CreateEvent from "./Modules/CreateEvent/CreateEvent";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <CreateEvent />
+    </div>
+  );
 }
 
 export default App;
